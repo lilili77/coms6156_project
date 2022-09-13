4a58a4f2745149dcebc38c216e5cac21f3f37cc3
lilili77 <lsjohnli7@gmail.com>
2022-09-13 16:59:22 -0400

    rename

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from cc_fall2022.cc_fall2022_stack import CcFall2022Stack
+from stack.main_stack import MainStack
 
 
 app = cdk.App()
-CcFall2022Stack(app, "CcFall2022Stack",
+MainStack(app, "COMS6156ProjectStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -15,12 +15,12 @@ CcFall2022Stack(app, "CcFall2022Stack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='074126400183', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
--- tests/unit/test_cc_fall2022_stack.py
+++ tests/unit/test_cc_fall2022_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from cc_fall2022.cc_fall2022_stack import CcFall2022Stack
+from stack.main_stack import MainStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cc_fall2022/cc_fall2022_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = CcFall2022Stack(app, "cc-fall2022")
+    stack = MainStack(app, "cc-fall2022")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
