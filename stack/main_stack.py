4a7b7088bc32f54cdd76d60415a142ce60651ba8
lilili77 <lsjohnli7@gmail.com>
2022-10-18 22:10:27 -0400

    Setup Fargate to Run Dockerized Flask App

--- stack/main_stack.py
+++ stack/main_stack.py
@@ -2,6 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_ec2, aws_ecs, aws_ecs_patterns,
+    CfnOutput
 )
 from constructs import Construct
 
@@ -12,8 +14,17 @@ class MainStack(Stack):
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CcFall2022Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        vpc = aws_ec2.Vpc(self, 'MyVPC', cidr='10.0.0.0/16')
+
+        cluster = aws_ecs.Cluster(self, 'MyCluster', vpc=vpc)
+        load_balanced_fargate_service = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "FargateService",
+            cluster=cluster,
+            task_image_options=aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=aws_ecs.ContainerImage.from_asset('flaskapp') # .from_registry("amazon/amazon-ecs-sample")
+                
+            )
+        )
+
+        CfnOutput(self, 'SericeURL', value="http://{}".format(
+            load_balanced_fargate_service.load_balancer.load_balancer_dns_name))
