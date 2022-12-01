9a68b4e6685dba14eb0d3316386c16fb2fc50ea9
Tina Chen <tobigxx@hotmail.com>
2022-11-30 21:56:36 -0500

    fix(video): Schema Update
    added image url field

--- microservices/video/model.py
+++ microservices/video/model.py
@@ -10,19 +10,21 @@ class VideoModel(sa.Model):
     __tablename__ = 'video'
     id = sa.Column(sa.Integer, primary_key=True)
     video_url = sa.Column(sa.String(500))
+    image_url = sa.Column(sa.String(500))
     name = sa.Column(sa.String(80))
     actor = sa.Column(sa.String(200))
     length = sa.Column(sa.String(200))
     genre = sa.Column(sa.String(80))
     review_rating = sa.Column(sa.Float)
 
-    def __init__(self, video_url, name, actor, length, genre, review_rating):
+    def __init__(self, video_url, name, actor, length, genre, review_rating, image_url):
         self.video_url = video_url
         self.name = name
         self.actor = actor
         self.length = length
         self.genre = genre
         self.review_rating = review_rating
+        self.image_url = image_url
 
 
 class VideoSchema(ma.Schema):
@@ -33,3 +35,4 @@ class VideoSchema(ma.Schema):
     length = fields.String(required=True)
     genre = fields.String(required=False)
     review_rating = fields.Float(required=False)
+    image_url = fields.String(required=False)
