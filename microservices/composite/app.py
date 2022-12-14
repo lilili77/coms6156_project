fe872b722255b1d0f36c783c22d71a4d44d1321f
Evstrife <siyuwu0714@gmail.com>
2022-12-14 17:32:16 -0500

    email notification added

--- microservices/composite/app.py
+++ microservices/composite/app.py
@@ -78,84 +78,40 @@ def get_users_from_room(current_room_id):
     return response_user.text
 
 
-@app.route('/room-user/sns')
+@app.route('/room-user/invite/<int:host_id>/<int:guest_id>/<int:room_id>', methods=['GET')
 def sns_test():
+    # get invitation link?
+    invite_url = os.enviton['ApiURL']'/room-user/join_room/<int:user_id>'
+    # get host username
+    host_url = os.environ['ApiURL'] + "/user/users/" + str(host_id)
+    headers = requests.utils.default_headers()
+    response_host = requests.get(host_url, headers=headers).json()
+    host_username = response_host['data']['username']
+    # get guest username and email
+    guest_url = os.environ['ApiURL'] + "/user/users/" + str(guest_id)
+    headers = requests.utils.default_headers()
+    response_guest = requests.get(guest_url, headers=headers).json()
+    guest_username = response_guest['data']['username']
+    guest_email = response_guest['data']['email']
+    # generate email
+    message = "Hi"+ guest_username + "!" + host_username + "is inviting you to watch a video on Zoomflex! The room id is " + str(room_id)
+    publishobject = {'Message': message,
+                     'Invitationlink': invite_url
+                    }
+    # Add sns subscriber                
+    # client.subscribe(
+    #     TopicArn = os.environ['TopicARN'],
+    #     Protocal='sns',
+    #     Endpoint = email
+    # )
+    # publish a email
     sns_client = boto3.client('sns', region_name='us-east-1')
     r = sns_client.publish(TopicArn=os.environ['TopicARN'],
-                           Message=json.dumps({'test': 'test'}),
-                           Subject="Something happend")
+                           Message=json.dumps(publishobject),
+                           Subject="Invitation to join Zoomflex room")
     return r
 
-@app.route('/room-user/ses')
-def ses():
-    
-    SENDER = "zoomflex6156@gmail.com" 
-    RECIPIENT = "zoomflex6156@gmail.com" # must be verified in AWS SES Email
 
-    AWS_REGION = "us-east-1"
-
-    # The subject line for the email.
-    SUBJECT = "This is test email for testing purpose..!!"
-
-    # The email body for recipients with non-HTML email clients.
-    BODY_TEXT = ("You are invited to a room in zoomflex."
-                ""
-                )
-                
-    # The HTML body of the email.
-    BODY_HTML = """<html>
-    <head></head>
-    <body>
-    <h1>Hey Hi...</h1>
-    <p>This email was sent with
-        <a href='https://aws.amazon.com/ses/'>Amazon SES CQPOCS</a> using the
-        <a href='https://aws.amazon.com/sdk-for-python/'>
-        AWS SDK for Python (Boto)</a>.</p>
-    </body>
-    </html>
-                """            
-
-    # The character encoding for the email.
-    CHARSET = "UTF-8"
-
-    # Create a new SES resource and specify a region.
-    client = boto3.client('ses',region_name=AWS_REGION)
-
-    # Try to send the email.
-    try:
-        #Provide the contents of the email.
-        response = client.send_email(
-            Destination={
-                'ToAddresses': [
-                    RECIPIENT,
-                ],
-            },
-            Message={
-                'Body': {
-                    'Html': {
-        
-                        'Data': BODY_HTML
-                    },
-                    'Text': {
-        
-                        'Data': BODY_TEXT
-                    },
-                },
-                'Subject': {
-
-                    'Data': SUBJECT
-                },
-            },
-            Source=SENDER
-        )
-    
-    # Display an error if something goes wrong.	
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        print("Email sent! Message ID:"),
-        print(response['MessageId'])
-        
 
 
 if __name__ == "__main__":
